a1fe8530129358f065f7284fc99cec9db1adde00
Rafael Soares <r.soares2015@outlook.com>
2023-02-28 16:46:07 -0300

    feat: Criacao da model de User

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,28 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import IUser from '../../interfaces/IUser';
+import db from '.';
+
+export default class UserModel extends Model implements IUser {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+UserModel.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: STRING,
+  role: STRING,
+  email: STRING,
+  password: STRING,
+}, {
+  sequelize: db,
+  timestamps: false,
+  underscored: true,
+});
