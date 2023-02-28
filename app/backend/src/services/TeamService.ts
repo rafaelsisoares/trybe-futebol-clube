e7b864aa7bc9c03f5185395bc105b87587f8b13d
Rafael Soares <r.soares2015@outlook.com>
2023-02-27 22:39:09 -0300

    fix: Correcoes no service para que os dados retorne corretamente

--- app/backend/src/services/TeamService.ts
+++ app/backend/src/services/TeamService.ts
@@ -6,7 +6,8 @@ export default class TeamService {
   private _model: ModelStatic<TeamModel> = TeamModel;
 
   async findAll(): Promise<ITeam[]> {
-    const teams = await this._model.findAll();
+    const result = await this._model.findAll();
+    const teams: ITeam[] = result.map((team) => team.dataValues);
     return teams;
   }
 }
