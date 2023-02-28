57af8f11da7c7a01cf398489b8a445c8be2f9c1f
Rafael Soares <r.soares2015@outlook.com>
2023-02-28 00:18:34 -0300

    feat: Criacao do endpoint GET /teams/:id

--- app/backend/src/routes/TeamRouter.ts
+++ app/backend/src/routes/TeamRouter.ts
@@ -5,5 +5,6 @@ const router = Router();
 const teamController = new TeamController();
 
 router.get('/', (req, res) => teamController.findAll(req, res));
+router.get('/:id', (req, res) => teamController.findById(req, res));
 
 export default router;
