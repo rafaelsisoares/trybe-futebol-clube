39e3e310f464ddbad68b8e86e23ea98795d828e2
Rafael Soares <r.soares2015@outlook.com>
2023-02-28 19:17:56 -0300

    feat: Criacao do service de User

---
+++ app/backend/src/services/UserService.ts
@@ -0,0 +1,26 @@
+import { ModelStatic } from 'sequelize';
+import * as bcrypt from 'bcryptjs';
+import UserModel from '../database/models/UserModel';
+import { ILogin } from '../interfaces/IUser';
+import generateToken from '../utils/generateToken';
+
+export default class UserService {
+  private _model: ModelStatic<UserModel> = UserModel;
+
+  async login(data: ILogin) {
+    const { email, password } = data;
+
+    const result = await this._model.findOne({
+      where: { email },
+    });
+
+    if (!result) throw new Error('Invalid email or password');
+
+    if (bcrypt.compareSync(password, result.dataValues.password)) {
+      throw new Error('Invalid email or password');
+    }
+
+    const token = generateToken(result.dataValues);
+    return token;
+  }
+}
