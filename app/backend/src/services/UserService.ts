e376d63b0cf941a3119063c39cca70cc9391f042
Rafael Soares <r.soares2015@outlook.com>
2023-03-01 01:04:06 -0300

    feat: Criacao de schema para validacao de email e password

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -3,12 +3,18 @@ import * as bcrypt from 'bcryptjs';
 import UserModel from '../database/models/UserModel';
 import { ILogin } from '../interfaces/IUser';
 import generateToken from '../utils/generateToken';
+import loginDataSchema from './validators/schemas';
 
 export default class UserService {
   private _model: ModelStatic<UserModel> = UserModel;
 
   async login(data: ILogin) {
     const { email, password } = data;
+
+    const { error } = loginDataSchema.validate(data);
+    if (error) {
+      throw new Error('Invalid email or password');
+    }
 
     const result = await this._model.findOne({
       where: { email },
