c6b116ad2ae86a496ec810e12d4a577f53348afd
Rafael Soares <r.soares2015@outlook.com>
2023-03-01 19:57:20 -0300

    feat: Criacao da rota GET /login/role

--- app/backend/src/routes/UserRouter.ts
+++ app/backend/src/routes/UserRouter.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import checkLoginInput from '../middlewares/checkLoginInput';
 
 import UserController from '../controllers/UserController';
+import checkToken from '../middlewares/checkToken';
 
 const router = Router();
 const userController = new UserController();
 
 router.post('/', checkLoginInput, (req, res) => userController.login(req, res));
+router.get('/role', checkToken);
 
 export default router;
