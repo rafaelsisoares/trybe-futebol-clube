388cf578cd6eb693fdabe3a9710a158406fec01f
Rafael Soares <r.soares2015@outlook.com>
2023-02-28 22:15:49 -0300

    feat: Criacao do middleware de validacao dos dados de login

--- app/backend/src/routes/UserRouter.ts
+++ app/backend/src/routes/UserRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import checkLoginInput from '../middlewares/checkLoginInput';
 
 import UserController from '../controllers/UserController';
 
 const router = Router();
 const userController = new UserController();
 
-router.post('/', (req, res) => userController.login(req, res));
+router.post('/', checkLoginInput, (req, res) => userController.login(req, res));
 
 export default router;
