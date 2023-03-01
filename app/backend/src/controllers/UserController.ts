9df4866bf34f0ba2a14219eb68aaa7a9c7729b47
Rafael Soares <r.soares2015@outlook.com>
2023-02-28 22:24:37 -0300

    fix: Corrigido retorno da response

--- app/backend/src/controllers/UserController.ts
+++ app/backend/src/controllers/UserController.ts
@@ -8,7 +8,7 @@ export default class UserController {
   async login(req: Request, res: Response) {
     try {
       const token = await this._service.login(req.body);
-      res.status(200).json(token);
+      res.status(200).json({ token });
     } catch ({ message }) {
       res.status(400).json({ message });
     }
