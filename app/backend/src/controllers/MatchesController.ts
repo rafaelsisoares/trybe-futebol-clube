9a7d91fd1a65d0c79139fc329c26f45b1363126e
Rafael Soares <r.soares2015@outlook.com>
2023-03-02 20:17:20 -0300

    feat: Criacao de filtro de busca entre partidas em andamento ou finalizadas

--- app/backend/src/controllers/MatchesController.ts
+++ app/backend/src/controllers/MatchesController.ts
@@ -4,8 +4,10 @@ import MatchesService from '../services/MatchesService';
 export default class MatchesController {
   private _service = new MatchesService();
 
-  async findAll(req: Request, res: Response) {
-    const matches = await this._service.findAll();
+  async findAll(req: Request, res: Response): Promise<void | Response> {
+    const { inProgress } = req.query;
+
+    const matches = await this._service.findAll(inProgress as string);
     res.status(200).json(matches);
   }
 }
