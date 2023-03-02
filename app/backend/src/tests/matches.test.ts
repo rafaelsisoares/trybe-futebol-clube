bea9a8943787be5bbb315be0f79018347c0c26f7
Rafael Soares <r.soares2015@outlook.com>
2023-03-02 17:29:51 -0300

    feat: Criacao dos testes do endpoint /matches

---
+++ app/backend/src/tests/matches.test.ts
@@ -0,0 +1,34 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import MatchesModel from '../database/models/MatchesModel';
+
+import { Response } from 'superagent';
+import matches from './mocks/matches.mock';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testes do endpoint /matches', () => {
+   before(async () => {
+     sinon.stub(MatchesModel, "findAll")
+      .resolves(matches as unknown as MatchesModel[]);
+  });
+
+   after(()=>{
+   (MatchesModel.findAll as sinon.SinonStub).restore();
+  });
+
+  describe('Testa se', () => {
+    it('retorna todas as partidas', async () => {
+      await chai.request(app).get('/matches').then((res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('array');
+      });
+    });
+  });
+});
