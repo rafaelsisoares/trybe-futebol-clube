a32f6bd954284d6fb66510153f46d6ab25dc3749
Rafael Soares <r.soares2015@outlook.com>
2023-03-03 18:42:34 -0300

    feat: Adicao do middleware checkTeams

--- app/backend/src/routes/MatchesRouter.ts
+++ app/backend/src/routes/MatchesRouter.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import checkToken from '../middlewares/checkToken';
 import MatchesController from '../controllers/MatchesController';
+import checkTeams from '../middlewares/checkTeams';
 
 const router = Router();
 const matchesController = new MatchesController();
 
 router.get('/', (req, res) => matchesController.findAll(req, res));
-router.post('/', checkToken, (req, res) => matchesController.createMatch(req, res));
+router.post('/', checkToken, checkTeams, (req, res) => matchesController.createMatch(req, res));
 router.patch('/:id', checkToken, (req, res) => matchesController.updateMatch(req, res));
 router.patch('/:id/finish', checkToken, (req, res) => matchesController.finishMatch(req, res));
 
