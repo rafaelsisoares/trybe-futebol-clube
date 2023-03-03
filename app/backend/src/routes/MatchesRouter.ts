58fb014960d82d0306e984a0889762fae2631470
Rafael Soares <r.soares2015@outlook.com>
2023-03-02 22:43:57 -0300

    feat: Uso do middleware checkToken na rota/matches/:id/finish

--- app/backend/src/routes/MatchesRouter.ts
+++ app/backend/src/routes/MatchesRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import checkToken from '../middlewares/checkToken';
 import MatchesController from '../controllers/MatchesController';
 
 const router = Router();
 const matchesController = new MatchesController();
 
 router.get('/', (req, res) => matchesController.findAll(req, res));
+router.patch('/:id/finish', checkToken, (req, res) => matchesController.finishMatch(req, res));
 
 export default router;
