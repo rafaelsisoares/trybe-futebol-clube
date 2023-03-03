5dbdd5a10c64b242b6dc52cdfc7e60d2054848bc
Rafael Soares <r.soares2015@outlook.com>
2023-03-02 21:00:58 -0300

    feat: Criacao do metodo finishMatch no controller

--- app/backend/src/controllers/MatchesController.ts
+++ app/backend/src/controllers/MatchesController.ts
@@ -10,4 +10,13 @@ export default class MatchesController {
     const matches = await this._service.findAll(inProgress as string);
     res.status(200).json(matches);
   }
+
+  async finishMatch(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const isFinished = await this._service.finishMatch(+id);
+
+    if (!isFinished) return res.status(500).json({ message: 'Internal Server Error' });
+
+    return res.status(200).json({ message: 'Finished' });
+  }
 }
