9e08ecb3600e65231c7137da8657aa1328f1d0df
Rafael Soares <r.soares2015@outlook.com>
2023-03-02 23:19:09 -0300

    feat: Criacao da rota PATCH /matches/:id

--- app/backend/src/routes/MatchesRouter.ts
+++ app/backend/src/routes/MatchesRouter.ts
@@ -6,6 +6,7 @@ const router = Router();
 const matchesController = new MatchesController();
 
 router.get('/', (req, res) => matchesController.findAll(req, res));
+router.patch('/:id', checkToken, (req, res) => matchesController.updateMatch(req, res));
 router.patch('/:id/finish', checkToken, (req, res) => matchesController.finishMatch(req, res));
 
 export default router;
