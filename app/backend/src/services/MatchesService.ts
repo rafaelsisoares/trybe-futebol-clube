221040fa857b59788bfacfa4f0a0818cafd4cb03
Rafael Soares <r.soares2015@outlook.com>
2023-03-03 17:13:32 -0300

    feat: Criacao das validacoes dos ids dos times

--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -4,9 +4,12 @@ import TeamModel from '../database/models/TeamModel';
 import MatchesModel from '../database/models/MatchesModel';
 import { IMatch } from '../interfaces/IMatch';
 import IMatchInput from '../interfaces/IMatchInput';
+import IErrorReturn from '../interfaces/IErrorReturn';
+import TeamService from './TeamService';
 
 export default class MatchesService {
   private _model: ModelStatic<MatchesModel> = MatchesModel;
+  private _teamService: TeamService = new TeamService();
 
   async findAll(query?: string | undefined): Promise<IMatch[]> {
     const result = await this._model.findAll({
@@ -51,8 +54,29 @@ export default class MatchesService {
     return updatedMatch as IMatch;
   }
 
-  async createMatch(newMatch: INewMatch): Promise<IMatch> {
+  private async findTeams(homeId: number, awayId: number): Promise<boolean> {
+    const homeTeam = await this._teamService.findById(homeId);
+    const awayTeam = await this._teamService.findById(awayId);
+
+    if (!homeTeam || !awayTeam) return false;
+
+    return true;
+  }
+
+  async createMatch(newMatch: INewMatch): Promise<IMatch | IErrorReturn> {
     const { homeTeamId, homeTeamGoals, awayTeamId, awayTeamGoals } = newMatch;
+
+    if (homeTeamId === awayTeamId) {
+      return {
+        status: 422,
+        message: 'It is not possible to create a match with two equal teams',
+      };
+    }
+
+    if (!this.findTeams(homeTeamId, awayTeamId)) {
+      return { status: 404, message: 'There is no team with such id!' };
+    }
+
     const result = await this._model.create({
       homeTeamId,
       homeTeamGoals,
