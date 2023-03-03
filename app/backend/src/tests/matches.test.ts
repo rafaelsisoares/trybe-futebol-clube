38b9bb5ce778f28a9faa540938bc6748044adec4
Rafael Soares <r.soares2015@outlook.com>
2023-03-03 15:23:28 -0300

    test: Criacao do teste de criacao da partida

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -7,21 +7,31 @@ import { app } from '../app';
 import MatchesModel from '../database/models/MatchesModel';
 
 import { Response } from 'superagent';
-import matches from './mocks/matches.mock';
+import { matches, correctReturn, token, validMatch } from './mocks/matches.mock';
+import generateToken from '../utils/generateToken';
+import MatchesService from '../services/MatchesService';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
 describe('Testes do endpoint /matches', () => {
+  const inProgress = matches.filter((match) => match.inProgress === 1);
+  const finished = matches.filter((match) => match.inProgress === 0);
   describe('Testa se', () => {
-    before(async () => {
+    /* before(async () => {
       sinon.stub(MatchesModel, "findAll")
        .resolves(matches as unknown as MatchesModel[]);
-   });
+       sinon.stub(MatchesModel, "findAll")
+       .resolves(inProgress as unknown as MatchesModel[]);
+        sinon.stub(MatchesModel, "findAll")
+        .resolves(finished as unknown as MatchesModel[]);
+   }); */
  
-    after(()=>{
-    (MatchesModel.findAll as sinon.SinonStub).restore();
+    afterEach(()=>{
+    /* (MatchesModel.findAll as sinon.SinonStub).restore();
+    (MatchesModel.findAll as sinon.SinonStub).restore(); */
+    sinon.restore();
    });
 
     it('retorna todas as partidas', async () => {
@@ -29,6 +39,24 @@ describe('Testes do endpoint /matches', () => {
         expect(res).to.have.status(200);
         expect(res.body).to.be.an('array');
       });
+    });
+
+    it('retorna todas as partidas em andamento', async () => {
+      await chai.request(app).get('/matches?inProgress=true').then((res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.be.eq(inProgress);
+      });
+    });
+
+    it('é possível criar uma partida', async () => {
+      sinon.stub(MatchesModel, 'create').resolves(correctReturn as MatchesModel);
+      await chai.request(app).post('/matches').set('authorization', token)
+      .send(validMatch)
+      .then((res) => {
+        expect(res).to.have.status(201);
+        expect(res.body).to.be.eq(correctReturn);
+      })
     });
   });
 });
