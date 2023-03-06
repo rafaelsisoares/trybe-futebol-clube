3aab3dea3642e19d3bfdbbfb3570ebed084850b0
Rafael Soares <r.soares2015@outlook.com>
2023-03-06 16:07:01 -0300

    feat: Criacao do metodo getAwayLeaderboard no controller

--- app/backend/src/controllers/LeaderboardController.ts
+++ app/backend/src/controllers/LeaderboardController.ts
@@ -4,8 +4,13 @@ import LeaderboardService from '../services/LeaderboardService';
 export default class LeaderboardController {
   private _service = new LeaderboardService();
 
-  async getHomeLeaderboard(req: Request, res: Response): Promise<Response> {
+  async getHomeLeaderboard(_req: Request, res: Response): Promise<Response> {
     const leaderboard = await this._service.leaderboard('homeTeamId');
+    return res.status(200).json(leaderboard);
+  }
+
+  async getAwayLeaderboard(_req: Request, res: Response): Promise<Response> {
+    const leaderboard = await this._service.leaderboard('awayTeamId');
     return res.status(200).json(leaderboard);
   }
 }
