9b28d14fe678c45d0fb6dd98b4f166be5a27b0fb
Rafael Soares <r.soares2015@outlook.com>
2023-03-11 22:11:33 -0300

    fix: Correcao nos testes do endpoint /teams

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -16,27 +16,27 @@ const { expect } = chai;
 describe('Testes do endpoint /teams', () => {
   let chaiHttpResponse: Response;
 
-  before(async () => {
-    sinon.stub(TeamModel, 'findAll').resolves(teams as TeamModel[]);
-  });
-
-  after(() => {
-    (TeamModel.findAll as sinon.SinonStub).restore();
+  afterEach(() => {
+    sinon.restore();
   });
 
     it('Testa se retorna todos os times', async () => {
+      sinon.stub(TeamModel, 'findAll').resolves(teams as TeamModel[]);
       chaiHttpResponse = await chai.request(app).get('/teams');
       expect(chaiHttpResponse.status).to.be.eq(200);
       expect(chaiHttpResponse.body).to.be.an('array');
     });
 
     it('Testa se retorna um erro caso não exista um time com o id passado', async () => {
+      sinon.stub(TeamModel, 'findAll').resolves(undefined);
       chaiHttpResponse = await chai.request(app).get('/teams/1000');
-      expect(chaiHttpResponse.body).to.be.eq({ message: 'Team not found' });
+      expect(chaiHttpResponse.status).to.be.eq(404);
+      expect(chaiHttpResponse.body).to.deep.eq({ message: 'Team not found' });
     });
 
     it('Testa se retorna um time pelo seu id', async () => {
       chaiHttpResponse = await chai.request(app).get('/teams/10');
       expect(chaiHttpResponse.status).to.be.eq(200);
+      expect(chaiHttpResponse.body).to.deep.eq(teams[9]);
     });
 });
